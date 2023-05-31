a74a2bb3f0a0621312dc058649de4e5d52bf2c4f
siniyq <siniy.by@mail.ru>
2023-05-31 19:40:35 +0300

    init project

---
+++ src/main/java/com/items/items/ItemsApplication.java
@@ -0,0 +1,14 @@
+package com.items.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ItemsApplication.class, args);
+	}
+
+}
